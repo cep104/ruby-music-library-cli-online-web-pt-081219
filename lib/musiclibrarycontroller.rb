e4912a3b03759f64ddcf3ca37ae4bcf842fb4a1f
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-26 20:30:05 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -46,6 +46,7 @@ def list_songs_by_artist
  artist = Artist.find_by_name(input)
  artist.songs.uniq.sort{|a,b| a.name <=> b.name}.each_with_index do |song,index| 
     puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
 end
 
 end
