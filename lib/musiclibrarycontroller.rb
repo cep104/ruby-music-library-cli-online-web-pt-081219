0fc22664d79c921231d91a00fa3c27f00afb6a73
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-26 21:01:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -64,6 +64,7 @@ def play_song
   puts "Which song number would you like to play?"
   input = gets.chomp
   if list_songs.include?(input)
+    song = Song.all
     puts "Playing #{song.name}"
 end
 end
